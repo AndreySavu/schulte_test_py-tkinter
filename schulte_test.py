2ffb10dfcf7b5287857463b3d8670df4df1e68d6
Andrey <Andrey@DESKTOP-KQ0LS2R>
2023-10-17 23:26:29 +0700

    testinggg

--- schulte_test.py
+++ schulte_test.py
@@ -5,7 +5,7 @@ from frames.StartWindow import StartWindow
 
 class App:
         def __init__(self):
-            #объект Tkinter----
+            #объект Tkinter------
             self.root = Tk()
             self.root.geometry('1000x600')
             self.root.title("Тестирование Шульте")
