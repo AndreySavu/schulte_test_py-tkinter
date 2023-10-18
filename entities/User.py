04d1423066fd07a8ab5be8368cad3e2fedff6253
George <George@211.ru>
2023-10-18 20:02:22 +0700

    some changes

--- entities/User.py
+++ entities/User.py
@@ -1,18 +1,29 @@
-class User():
-    def __init__(self, name, age, typ):
-        self.name = name
-        self.age = age
-        self.type = typ
-    
-    def set_name(self, name):
-        self.name = name
-    def set_age(self, age):
-        self.age = age
-    def set_type(self, typ):
-        self.type = typ
-    def get_name(self):
-        return self.name
-    def get_age(self):
-        return self.age
-    def get_type(self):
-        return self.type
+class User:
+    def __init__(self, name, age, type):
+        self._name = name
+        self._age = age
+        self._type = type
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def age(self):
+        return self._age
+
+    @property
+    def type(self):
+        return self._type
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+    @type.setter
+    def type(self, type):
+        self._type = type
--- schulte_test.py
+++ schulte_test.py
@@ -5,16 +5,17 @@ from frames.StartWindow import StartWindow
 
 
 class App:
-        def autoc(self):
-            self.time_l.configure(text= str(int(time.time()) - self.start_time) )
-            self.root.after(1000, self.autoc)
+    def __init__(self):
+        # объект Tkinter------
+        self.root = Tk()
+        self.root.geometry('1000x600')
+        self.root.title("Тестирование Шульте")
+        start = StartWindow(self.root)
+        self.root.mainloop()
 
-        def __init__(self):
-            #объект Tkinter------
-            self.root = Tk()
-            self.root.geometry('1000x600')
-            self.root.title("Тестирование Шульте")
-            start = StartWindow(self.root)
-            self.root.mainloop()
+    def autoc(self):
+        self.time_l.configure(text=str(int(time.time()) - self.start_time))
+        self.root.after(1000, self.autoc)
+
 
 app = App()
